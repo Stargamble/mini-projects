30a48c1db75c3a363c08866f5d8d5f66c8ffefa4
Stargamble <163446011+Stargamble@users.noreply.github.com>
2024-12-22 09:45:29 -0800

    Add files via upload

---
+++ Program (10).cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CityTest
+{
+    public class City
+    {
+
+        private string name;
+        private int population;
+        private string country;
+
+        
+        public City(string name, int population)
+        {
+            this.name = name;
+            this.population = population;
+        }
+
+        public City(string name, int population, string country)
+        {
+            this.name = name;
+            this.population = population;
+            this.country = country;
+        }
+
+        public string GetCity()
+        {
+            return name;
+        }
+
+        public int GetPopulation()
+        {
+            return population;
+        }
+
+      
+        public string Country
+        {
+            get { return country; }
+        }
+
+
+        public static void DisplayInfo()
+        {
+            Console.WriteLine("City Info Version 1.0");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            City.DisplayInfo();
+            City myCity = new City("Seattle", 750000, "USA");
+
+            Console.WriteLine($"City: {myCity.GetCity()}");
+            Console.WriteLine($"Population: {myCity.GetPopulation()}");
+            Console.WriteLine($"Country: {myCity.Country}");
+        }
+    }
+}
+
+
